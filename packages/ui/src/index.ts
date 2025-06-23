a67bcec9f15ad412f01b7e2ac83d7b780a09d719
Laís Batista <lais.pereira.hyti@caixaconsorcio.com.br>
2025-06-23 15:46:51 -0300

    ✨ chore: add new icons to the icon export module.

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -4,3 +4,4 @@ export * from "./components/ModalCaixa";
 export * from "./components/RadioButton";
 export * from "./components/Stepper";
 export * from "./components/SvgIcon";
+export * from "./assets/icons";
