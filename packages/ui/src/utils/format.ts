bf7c72beafa84769ce57893d38bfbfd960b62046
Edson Cândido <edson.alves.millenium@caixaconsorcio.com.br>
2025-07-07 23:33:35 -0300

    feat/CurrencySlider

--- packages/ui/src/utils/format.ts
+++ packages/ui/src/utils/format.ts
@@ -5,3 +5,11 @@ export const numberFormater = (value: number, fractionDigits: number = 2) => {
         maximumFractionDigits: fractionDigits
     }).format(value);
 }
+export const numberForMoney = (value: number, fractionDigits: number = 2) => {
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: fractionDigits,
+        maximumFractionDigits: fractionDigits
+    }).format(value);
+};
